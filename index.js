cfbf875110b64eba6c751547a6fac4a2ac8bbb95
yanapeshko <yanapeshko917@gmail.com>
2021-10-06 01:31:24 +0300

    color-switcher

---
+++ index.js
@@ -0,0 +1,43 @@
+"use strict";
+const refs = {
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+  body: document.querySelector("body"),
+};
+
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const timer = {
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+
+    this.isActive = true;
+    this.switchColors = setInterval(() => {
+      const min = 0;
+      const max = colors.length - 1;
+      let i = randomIntegerFromInterval(min, max);
+      refs.body.style.backgroundColor = colors[i];
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.switchColors);
+    this.isActive = false;
+  },
+};
+
+refs.startBtn.addEventListener("click", timer.start.bind(timer));
+refs.stopBtn.addEventListener("click", timer.stop.bind(timer));
